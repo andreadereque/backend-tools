ab6e6de26ca388b231de493e395cd2ea472cecf4
andreadereque <aerdna.4422@gmail.com>
2025-06-08 08:37:36 -0600

    Config maven project

---
+++ src/test/java/com/andrea/backendtools/backendtools/BackendToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrea.backendtools.backendtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
